434441579ed7f1102b61e58d74e58d8350945e7c
Josue <martinezgarciajosue54@gmail.com>
2022-07-24 14:16:54 -0500

    Ejercicio con la clase String

---
+++ strings/stringEjercicio3.cpp
@@ -0,0 +1,23 @@
+/* Este codigo volte el orden de las letras en un string */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string texto;
+    string texto_volteado = "";
+
+    cout << "> Introduce el texto: ";
+    getline(cin,texto);
+
+    for(int i = texto.size(); i <= texto.size(); i--){
+        texto_volteado.push_back(texto[i]);
+    }
+
+    cout << "> El texto volteado es: " << texto_volteado;
+
+    /* Nota: Este codigo podria utilizarse para comprobar
+    si una palabra es palindroma. */
+}
